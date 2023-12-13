b4930574574b2fd5b25bf02c63f93c35f42fa4b4
Vadym <petrukvadd@gmail.com>
2023-12-13 22:08:29 +0200

    blog

---
+++ blog/src/main/java/com/blog/blog/repo/PostRepository.java
@@ -0,0 +1,7 @@
+package com.blog.blog.repo;
+
+import com.blog.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
